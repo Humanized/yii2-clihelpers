75368414e7ded91c6a4792ec3cdfa24973983b48
jeffrey.geyssens <jeffrey@humanized.be>
2016-01-19 19:21:31 +0100

    First swoosh

--- controllers/Controller.php
+++ controllers/Controller.php
@@ -46,7 +46,7 @@ class Controller extends \yii\console\Controller {
         $this->$out('[');
         $this->$out($status, (($this->_exitCode === 0 ? Console::FG_GREEN : Console::FG_RED)), Console::BOLD);
         $this->$out("]\t");
-        $this->$out($this->_msg, (Console::BG_BLUE));
+        $this->$out($this->_msg, Console::BG_BLUE);
         $this->stdout("\n");
     }
 
@@ -72,6 +72,39 @@ class Controller extends \yii\console\Controller {
         if (!Console::isRunningOnWindows()) {
             system('stty echo');
         }
+    }
+
+    /**
+     * Import a CSV file
+     * 
+     * Imports a CSV table, in to a database provided an array of configuration options
+     * 
+     * - modelClassPath string
+     * 
+     * - columnMap array<int>=string Map of the columns
+     * 
+     * Protected as such methods can be overwritten and or extended
+     * 
+     * @param array<mixed> $config
+     */
+    protected function importCSV($config)
+    {
+        $fileName = $config['fileName'];
+        $file = fopen($fileName, "r");
+        while (!feof($file)) {
+            $record = fgetcsv($file, 0, $config['delimter']);
+            if (isset($record[0])) {
+                $model = new $config['saveModel']($this->getRecordColumns($record, $config['attributeMap']));
+                $model->save();
+            } else {
+                break;
+            }
+        }
+    }
+
+    protected function getRecordColumns($line, $columnMap)
+    {
+        
     }
 
 }
