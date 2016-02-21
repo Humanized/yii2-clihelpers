b47fab98aadf4ad33eeb9e79693ea2e5005ebc25
Jeffrey Geyssens <jeffrey@humanized.be>
2016-02-21 19:45:51 +0100

    [MOD] DataTable unload

--- components/DataTable.php
+++ components/DataTable.php
@@ -5,10 +5,16 @@ namespace humanized\clihelpers\components;
 /**
  * A collection of static helper functions to implement the user management 
  */
-class DataTable {
+class DataTable
+{
 
     public $modelClass;
     public $data = [];
+
+    public function unloadCondtion($record)
+    {
+        return ['name' => $record['name']];
+    }
 
     public static function load()
     {
@@ -22,6 +28,32 @@ class DataTable {
             $model = new $instance->modelClass($record);
             try {
                 $model->save();
+            } catch (\Exception $exc) {
+                echo $exc->getMessage();
+            }
+        }
+        echo 'Complete';
+    }
+
+    public static function unload()
+    {
+
+        $class = get_called_class();
+
+        echo 'Loading data from file: ' . "$class \n";
+        $instance = new $class();
+        foreach ($instance->data as $record) {
+
+
+
+            try {
+                $unloadCondition = $instance->unloadCondition($record);
+                $class = $instance->modelClass;
+                $model = $class::findOne($unloadCondition);
+                if (isset($model)) {
+                    echo 'dropping record:' . var_dump($unloadCondition);
+                    $model->delete();
+                }
             } catch (\Exception $exc) {
                 echo $exc->getMessage();
             }
